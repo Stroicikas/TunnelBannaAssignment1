25a6b472e6c63c48cf2e18128ba76accfd831d89
DKxd <35139906+Stroicikas@users.noreply.github.com>
2024-03-10 14:55:05 +0100

    main

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <?php
+    $linje19 = ['Hagsätra', 'Rågsved', 'Högdalen', 'Bandhagen', 'Stureby', 'Svedmyra', 'Sockenplan', 'Enskede Gård', 'Globen', 'Gullmarsplan', 'Skanstull', 'Medborgarplatsen', 'Slussen', 'Gamla Stan', 'T-Centralen', 'Hötorget', 'Rådmansgatan',
+    'Odenplan', 'S:T Eriksplan', 'Fridhemsplan', 'Thorildsplan', 'Kristineberg', 'Alvik', 'Stora Mossen', 'Abrahamsberg', 'Brommaplan', 'Åkeshov', 'Ängbyplan', 'Islandstorget', 'Blackeberg', 'Råcksta', 'Vällingby', 'Johannelund', 'Hässelby Gård', 'Hässelby Strand'];
+    ?>
+    <div class="container">
+        <div class="form-container">
+            <form action="#" method="post">
+                <label for="start">From:</label><br>
+                <select name="start" id="start">
+                <?php
+                for ($i = 0; $i < count($linje19); $i++) {
+                    echo "<option value='$linje19[$i]'>$linje19[$i]</option>";
+                }
+                ?>
+                </select>
+                <br>
+                <br>
+                <label for="end">To:</label><br>
+                <select name="end" id="end">
+                <?php
+                for ($i = 0; $i < count($linje19); $i++) {
+                    echo "<option value='$linje19[$i]'>$linje19[$i]</option>";
+                }
+                ?>
+                </select>
+                <button type="submit" class="custom-button" name="submit">Submit</button>
+            </form>
+        </div>
+        <?php
+        if (isset($_POST["submit"])) {
+            $startingpoint = $_POST["start"];
+            $endingpoint = $_POST["end"];
+
+            $startind = array_search($startingpoint, $linje19);
+            $endind = array_search($endingpoint, $linje19);
+
+            $finaltime = abs($endind - $startind) * 3;
+        ?>
+        <div class="result">
+        Time you will spend traveling from <strong><?php echo $startingpoint; ?></strong> to <strong><?php echo $endingpoint; ?></strong> is <?php echo $finaltime; ?> minutes
+        </div>
+        <?php
+        } 
+        ?>
+    </div>
+</body>
+</html>
